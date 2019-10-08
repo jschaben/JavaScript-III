a48dbbfe23883228b820d7c98fcf11cf70dfb4fe
Josh Schaben <jschaben42@gmail.com>
2019-10-08 17:20:37 -0500

    i hate prototypes that wont work

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,14 +15,17 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
-function GameObject(attributes) {
-  this.createdAt = attributes.createdAt;
-  this.name = attributes.name;
-  this.dimensions = attributes.dimensions;
+
+function GameObject (attribute) {
+  this.createdAt = attribute.createdAt;
+  this.name = attribute.name;
+  this.dimensions = attribute.dimensions;
+  
 }
-GameObject.prototype.destroy = function () {
-  return `${this.name} was removed from the game.`;
-};
+
+GameObject.prototype.destroy = function()
+{return`${this.name} was removed from the game`;};
+
 /*
   === CharacterStats ===
   * healthPoints
